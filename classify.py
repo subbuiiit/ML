46947e26d9d5e70e7fc079c4392fd52974c6b94f
Subbu Veeravasarapu <subbu@users.fias.science>
2016-08-25 18:32:56 +0200

    added extra classification codess

--- classify.py
+++ classify.py
@@ -1,3 +1,22 @@
+## A tutorial script for classification algorithms using scikitlearn
+## prepared by Subbu Veeravasarapu 
+
+'''
+Concepts convered:
+(1) Decision Trees
+(2) Naive Bayes
+(3) Logistic regression
+(4) Nearest Neighbors (kNN)
+(5) SVM linear
+(6) SVM kernel
+### ensembles
+(7) Random Forests
+(8) AdaBoost
+## descriminiative 
+(9) LDA
+(10) QDA
+'''
+
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap
@@ -6,14 +25,12 @@ import sklearn
 from sklearn.datasets import make_moons
 from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-
 from sklearn.externals.six import StringIO
 import pydot
 
 
 
-### prepare dataset
+### dataset prepparation
 
 (X, y) = make_moons(n_samples=100, noise=0.3, random_state=0)
 X = StandardScaler().fit_transform(X) 
@@ -24,26 +41,34 @@ x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
 y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
+
+figure = plt.figure(figsize=(27,3))
 cm = plt.cm.RdBu
 cm_bright = ListedColormap(['#FF0000', '#0000FF'])
-ax = plt.subplot(1, 2, 1)
+nC = 12 ## number of subfigures per row
+ax = plt.subplot(1, nC, 1)
 ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright) 
 ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6)
 ax.set_xlim(xx.min(), xx.max())
 ax.set_ylim(yy.min(), yy.max())
 ax.set_xticks(())
 ax.set_yticks(())
+ax.set_title("data set")
 
 
 
-## classify using a decision tree 
+## classification  
+#########################################(1)################################################# 
 '''
+DECISION TREES::
 nonparametric discriminartive learning method. goal is to predict a binary tree based model that
 predicts the traget value by learning simple decision tules from the data. Given a training data (X, y), a decision tree recursively partitions the space such that samples with same lables are grouped together.
 
 Controling parameters are max_depth.
 loss functions used are gini/entropy to measure impurity of datasplits. 
 '''
+from sklearn.tree import DecisionTreeClassifier
+
 clf = DecisionTreeClassifier(max_depth=5)
 clf.fit(X_train, y_train)
 score = clf.score(X_test, y_test)
@@ -51,7 +76,7 @@ if hasattr(clf, "decision_function"):
    Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
 else:
      Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
-ax = plt.subplot(1, 2, 2)
+ax = plt.subplot(1, nC, 2)
 # Put the result into a color plot
 Z = Z.reshape(xx.shape)
 ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
@@ -69,5 +94,303 @@ ax.set_yticks(())
 ax.set_title("decision trees")
 ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
                 size=15, horizontalalignment='right')
+                
+   
+#########################################(2)#################################################            
+'''
+NAIVE BAYES::
+parametric generative learning algorithms, based on applying Bayes theorem with the "naive" assumption that features are independent. It ends in a relation that posterion proportional to prior and product of marginals.
+P(y|X) = p(y) prod_i( p(x_i | y) ) / Z
+
+y* = argmax prod_i( p(x_i | y) ) p(y)
+
+if p(x_i | y) follows gaussian, then it is gaussianNB. 
+''' 
+from sklearn.naive_bayes import GaussianNB
+               
+clf = GaussianNB()   
+clf.fit(X_train, y_train)
+score = clf.score(X_test, y_test)
+Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+ax = plt.subplot(1, nC, 3)
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+# Plot also the training points
+ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+# and testing points
+ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                   alpha=0.6)
+
+ax.set_xlim(xx.min(), xx.max())
+ax.set_ylim(yy.min(), yy.max())
+ax.set_xticks(())
+ax.set_yticks(())
+ax.set_title("naive bayes")
+ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+                
+#########################################(3)################################################# 
+'''
+LOGISTIC REGRESSION::
+also known as max entropy classifier. 
+class conditions are modelled as logit functions.
+p(y|X) = 1/ (1+exp{-(W.X+b)})
+goal is to find W, b; 
+optimization algorithms are newton-cg, lbfgs, sgd
+'''
+
+from sklearn.linear_model import LogisticRegression
+
+clf = LogisticRegression(C=1e5)
+clf.fit(X_train, y_train)
+score = clf.score(X_test, y_test)
+Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+ax = plt.subplot(1, nC, 4)
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+# Plot also the training points
+ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+# and testing points
+ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                   alpha=0.6)
+
+ax.set_xlim(xx.min(), xx.max())
+ax.set_ylim(yy.min(), yy.max())
+ax.set_xticks(())
+ax.set_yticks(())
+ax.set_title("logistic regression")
+ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+
+
+#########################################(4)################################################# 
+'''
+kNN: consider k nearest data points to decide the label for a new data point
+'''
+
+from sklearn.neighbors import KNeighborsClassifier
+
+clf = KNeighborsClassifier(15, weights='distance')
+clf.fit(X_train, y_train)
+score = clf.score(X_test, y_test)
+Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+ax = plt.subplot(1, nC, 5)
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+# Plot also the training points
+ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+# and testing points
+ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                   alpha=0.6)
+
+ax.set_xlim(xx.min(), xx.max())
+ax.set_ylim(yy.min(), yy.max())
+ax.set_xticks(())
+ax.set_yticks(())
+ax.set_title("kNN, k=15")
+ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+
+#########################################(5)################################################# 
+'''
+SVM: linearSVM
+
+TODO: study kernels, loss functions
+'''
+
+from sklearn.svm import SVC
+
+clf = SVC(kernel="linear", C=0.025)
+clf.fit(X_train, y_train)
+score = clf.score(X_test, y_test)
+Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+ax = plt.subplot(1, nC, 6)
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+# Plot also the training points
+ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+# and testing points
+ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                   alpha=0.6)
+
+ax.set_xlim(xx.min(), xx.max())
+ax.set_ylim(yy.min(), yy.max())
+ax.set_xticks(())
+ax.set_yticks(())
+ax.set_title("Linear SVM")
+ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+
+#########################################(6)################################################# 
+'''
+SVM: radial basis function SVM
+'''
+
+from sklearn.svm import SVC
+
+clf = SVC(kernel='rbf', gamma=2, C=1)
+clf.fit(X_train, y_train)
+score = clf.score(X_test, y_test)
+Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+ax = plt.subplot(1, nC, 7)
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+# Plot also the training points
+ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+# and testing points
+ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                   alpha=0.6)
+
+ax.set_xlim(xx.min(), xx.max())
+ax.set_ylim(yy.min(), yy.max())
+ax.set_xticks(())
+ax.set_yticks(())
+ax.set_title("RBF SVM")
+ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+
+
+
+##  ensemble methods
+#########################################(7)################################################# 
+'''
+RANDOM FORESTS::
+In random forests, each tree in the ensemble is built from a sample drawn with replacement (i.e., a bootstrap sample) from the training set. In addition, when splitting a node during the construction of the tree, the split that is chosen is no longer the best split among all features. Instead, the split that is picked is the best split among a random subset of the features. As a result of this randomness, the bias of the forest usually slightly increases (with respect to the bias of a single non-random tree) but, due to averaging, its variance also decreases, usually more than compensating for the increase in bias, hence yielding an overall better model.
+'''
+
+from sklearn.ensemble import RandomForestClassifier
+
+clf = RandomForestClassifier(n_estimators=10, max_depth=5, max_features=1)
+clf.fit(X_train, y_train)
+score = clf.score(X_test, y_test)
+Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+ax = plt.subplot(1, nC, 8)
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+# Plot also the training points
+ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+# and testing points
+ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                   alpha=0.6)
+
+ax.set_xlim(xx.min(), xx.max())
+ax.set_ylim(yy.min(), yy.max())
+ax.set_xticks(())
+ax.set_yticks(())
+ax.set_title("RandomForests")
+ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+
+
+
+
+
+
+#########################################(8)################################################# 
+'''
+ADA BOOST::
+'''
+from sklearn.ensemble import AdaBoostClassifier
+
+clf = AdaBoostClassifier()
+clf.fit(X_train, y_train)
+score = clf.score(X_test, y_test)
+Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+ax = plt.subplot(1, nC, 9)
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+# Plot also the training points
+ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+# and testing points
+ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                   alpha=0.6)
+
+ax.set_xlim(xx.min(), xx.max())
+ax.set_ylim(yy.min(), yy.max())
+ax.set_xticks(())
+ax.set_yticks(())
+ax.set_title("AdaBoost")
+ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+
+
+
+#########################################(9)################################################# 
+'''
+LINEAR DESCRMININANT ANALYSIS
+'''
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+clf = LinearDiscriminantAnalysis()
+clf.fit(X_train, y_train)
+score = clf.score(X_test, y_test)
+Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+ax = plt.subplot(1, nC, 10)
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+# Plot also the training points
+ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+# and testing points
+ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                   alpha=0.6)
+
+ax.set_xlim(xx.min(), xx.max())
+ax.set_ylim(yy.min(), yy.max())
+ax.set_xticks(())
+ax.set_yticks(())
+ax.set_title("LDA")
+ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+
+
+
+
+#########################################(10)################################################# 
+'''
+QUADRATIC DESCRMININANT ANALYSIS
+'''
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+clf = QuadraticDiscriminantAnalysis()
+clf.fit(X_train, y_train)
+score = clf.score(X_test, y_test)
+Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+ax = plt.subplot(1, nC, 11)
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+# Plot also the training points
+ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+# and testing points
+ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                   alpha=0.6)
+
+ax.set_xlim(xx.min(), xx.max())
+ax.set_ylim(yy.min(), yy.max())
+ax.set_xticks(())
+ax.set_yticks(())
+ax.set_title("QDA")
+ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+
+
+
+
+
 
 plt.show()
